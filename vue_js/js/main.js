fa8fd6551449e52def2bf788a326ea9f3c151671
Emiri-i <emiriiskw@gmail.com>
2021-02-18 21:50:30 +0900

    added method

--- vue_js/js/main.js
+++ vue_js/js/main.js
@@ -5,11 +5,27 @@
     var vm = new Vue({
         el: '#app',
         data: {
+            newItem:'',
             todos: [
                 'task1',
                 'task2',
                 'task3'
             ]
+        },
+        methods: {
+            // addItem:function(e) {
+            //     e.preventDefault();
+            //     this.todos.push(this.newItem);
+            // }
+            addItem:function() {
+                this.todos.push(this.newItem);
+                this.newItem='';
+            },
+            deletItem:function(index) {
+                if(confirm('are you sure?')) {
+                    this.todos.splice(index,1);
+                }
+            }
         }
     });
 })();
