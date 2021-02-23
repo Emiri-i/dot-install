dae9c93d4a139a34cb6f5cfccf44c27217a87994
Emiri-i <emiriiskw@gmail.com>
2021-02-24 00:04:04 +0900

    added computed

--- vue_js/js/main.js
+++ vue_js/js/main.js
@@ -6,17 +6,17 @@
         el: '#app',
         data: {
             newItem:'',
-            // todos: [{
-            //     title:'task 1',
-            //     isDone:false
-            // },{
-            //     title:'task 2',
-            //     isDone:false
-            // },{
-            //     title:'task 3',
-            //     isDone:true
-            // }]
-            todos: []
+            todos: [{
+                title:'task 1',
+                isDone:false
+            },{
+                title:'task 2',
+                isDone:false
+            },{
+                title:'task 3',
+                isDone:true
+            }]
+            // todos: []
         },
         methods: {
             addItem:function() {
@@ -31,6 +31,26 @@
                 if(confirm('are you sure?')) {
                     this.todos.splice(index,1);
                 }
+            },
+            purge: function() {
+                if(!confirm('delete finished?')){
+                    return;
+                }
+                // this.todos = this.todos.filter(function(todo) {
+                //     return todo.isDone = false;
+                // });
+                this.todos = this.remaining;
+            }
+        },
+        computed: {
+            remaining: function() {
+                return this.todos.filter(function(todo) {
+                        return !todo.isDone;
+                    });
+                // var items = this.todos.filter(function(todo) {
+                //     return todo.isDone = false;
+                // });
+                // return items.length;
             }
         }
     });
